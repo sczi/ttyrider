d9624e0068ff0d52271f227521da76b47b214ef7
Scott Zimmermann <szimmermann@isecpartners.com>
2015-04-28 17:03:02 -0400

    save and restore tty settings

--- ttyrider.c
+++ ttyrider.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <syscall.h>
 #include <fcntl.h>
+#include <termios.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -10,6 +11,44 @@
 #include <sys/user.h>
 #include <sys/ioctl.h>
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
+
+/* from The Linux Programming Interface */
+/* Place terminal referred to by 'fd' in raw mode (noncanonical mode
+   with all input and output processing disabled). Return 0 on success,
+   or -1 on error. If 'prevTermios' is non-NULL, then use the buffer to
+   which it points to return the previous terminal settings. */
+
+int
+ttySetRaw(int fd, struct termios *prevTermios)
+{
+    struct termios t;
+
+    if (tcgetattr(fd, &t) == -1)
+        return -1;
+
+    if (prevTermios != NULL)
+        *prevTermios = t;
+
+    t.c_lflag &= ~(ICANON | ISIG | IEXTEN | ECHO);
+                        /* Noncanonical mode, disable signals, extended
+                           input processing, and echoing */
+
+    t.c_iflag &= ~(BRKINT | ICRNL | IGNBRK | IGNCR | INLCR |
+                      INPCK | ISTRIP | IXON | PARMRK);
+                        /* Disable special handling of CR, NL, and BREAK.
+                           No 8th-bit stripping or parity error handling.
+                           Disable START/STOP output flow control. */
+
+    t.c_oflag &= ~OPOST;                /* Disable all output processing */
+
+    t.c_cc[VMIN] = 1;                   /* Character-at-a-time input */
+    t.c_cc[VTIME] = 0;                  /* with blocking */
+
+    if (tcsetattr(fd, TCSAFLUSH, &t) == -1)
+        return -1;
+
+    return 0;
+}
 
 void mirror_output(pid_t pid, int fd) {
     ptrace(PTRACE_ATTACH, pid, 0, 0);
@@ -44,6 +83,9 @@ void mirror_output(pid_t pid, int fd) {
 }
 
 int main(int argc, char **argv) {
+    struct termios prev;
+    ttySetRaw(STDIN_FILENO, &prev);
+
     /* mirror output in child */
     if(fork() == 0)
         mirror_output(atoi(argv[1]), atoi(argv[2]));
@@ -59,4 +101,7 @@ int main(int argc, char **argv) {
 
     /* wait on ptrace-ing child to finish */
     wait(0);
+
+    /* restore original tty settings */
+    tcsetattr(STDIN_FILENO, TCSANOW, &prev);
 }
